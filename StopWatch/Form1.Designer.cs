826f603f499c574d47c399b4aa59708652843ae7
Sergey Ermolaev <serj.planet@mail.ru>
2018-06-14 10:45:28 +0300

    Add files via upload

---
+++ StopWatch/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace StopWatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.start_stop = new System.Windows.Forms.Button();
+            this.label_time = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // start_stop
+            // 
+            this.start_stop.Location = new System.Drawing.Point(12, 12);
+            this.start_stop.Name = "start_stop";
+            this.start_stop.Size = new System.Drawing.Size(124, 37);
+            this.start_stop.TabIndex = 0;
+            this.start_stop.Text = "Start";
+            this.start_stop.UseVisualStyleBackColor = true;
+            this.start_stop.Click += new System.EventHandler(this.start_stop_Click);
+            // 
+            // label_time
+            // 
+            this.label_time.AutoSize = true;
+            this.label_time.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_time.Location = new System.Drawing.Point(158, 15);
+            this.label_time.Name = "label_time";
+            this.label_time.Size = new System.Drawing.Size(110, 25);
+            this.label_time.TabIndex = 1;
+            this.label_time.Text = "label_time";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 299);
+            this.Controls.Add(this.label_time);
+            this.Controls.Add(this.start_stop);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start_stop;
+        private System.Windows.Forms.Label label_time;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ StopWatch/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StopWatch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        DateTime date;
+        private void start_stop_Click(object sender, EventArgs e)
+        {
+            date = DateTime.Now;
+            timer1.Enabled = !timer1.Enabled;
+            start_stop.Text = timer1.Enabled ? "Stop" : "Start";
+
+            Timer timer = new Timer();
+            timer.Interval = 10;
+            timer.Tick += new EventHandler(timer1_Tick);
+            timer.Start();
+        
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            long tick = DateTime.Now.Ticks - date.Ticks;
+            DateTime stopWatch = new DateTime();
+
+            stopWatch = stopWatch.AddTicks(tick);
+            label_time.Text = String.Format("{0:HH:mm:ss:ff}", stopWatch);
+        }
+    }
+}
